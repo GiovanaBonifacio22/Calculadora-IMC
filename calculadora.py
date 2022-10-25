6a2311df90425aca21e9e1139ed9933336193d55
GiovanaBonifacio22 <116585932+GiovanaBonifacio22@users.noreply.github.com>
2022-10-25 16:41:50 -0300

    calculadora atualizada
    add informaçoes da tabela imc

--- calculadora.py
+++ calculadora.py
@@ -3,9 +3,9 @@ nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 peso = float(input("Digite seu peso: "))
-imc = peso/altura**2
+imc = peso/altura**2 # formula para calcular o imc
 
-
+# saldo de dados
 print("-" * 30)
 print("OS DADOS COLETADOS FORAM:")
 print("NOME",nome)
@@ -15,4 +15,21 @@ print("PESO",peso, "Kg")
 print("IMC: ", imc)
 print("-" * 30)
 
+if imc < 16:
+    print("Magreza grave")
+elif imc < 17:
+    print("Magreza moderada")
+elif imc < 18.5: 
+    print("Magreza leve")
+elif imc < 25: 
+    print("Saudavel")
+elif imc < 30: 
+    print("Sobrepeso")
+elif imc < 35: 
+    print("Obesidade grau I")
+elif imc < 40: 
+    print("Obesidade grau II")
+else: 
+    print("Obesidade grau III")
 
+print("-" * 30)   
